f4b3e10a7af95cc29f54fa3e2a7c879a137d931b
Amosi sanga <70682160+Amosi-04-sanga@users.noreply.github.com>
2021-11-07 00:31:04 +0300

    back to top functionality

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,26 @@
 // DOM element references
-
+const toTop = document.getElementById("to-top")
 
 
 
 
 
 // EVENT LISTNERS
-
+toTop.addEventListener("click", scrollToTop)
 
 
 
 
 
 // FUNCTIONS
-
+function scrollToTop() {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 8);
+    }
+}
+  
 
 
 
